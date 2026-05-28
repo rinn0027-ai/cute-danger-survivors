fb754cdc6f25f983ba4e70d52aa0b7248f9053d2
lin xiaowei <nunu@lindeMacBook-Air.local>
2026-05-28 19:20:53 +0800

    Export latest web build

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779960129|1206254';
+const CACHE_VERSION = '1779967055|1244992';
 /** @type {string} */
 const CACHE_PREFIX = 'Cute Danger Surv-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
